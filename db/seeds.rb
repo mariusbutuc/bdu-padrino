21fcf4dd695177b2996e2d9150a8f5c09366cafa
Marius Butuc <marius.butuc@gmail.com>
2012-12-31 22:33:31 -0500

    Add logos for downloadable IBM products

--- db/seeds.rb
+++ db/seeds.rb
@@ -311,9 +311,9 @@ news.each_with_index do |n, index|
 end
 
 downloads = [
-  { title: 'BigInsights Basic Edition', url: 'https://www14.software.ibm.com/webapp/iwm/web/preLogin.do?source=swg-ibmibee&S_CMP=BigI_BDUDWLDTAB', description: 'BigInsights Basic Edition is the free version of BigInsights which includes Hadoop and other additional resources.' },
-  { title: 'Streams', url: 'https://www14.software.ibm.com/webapp/iwm/web/preLogin.do?source=SWG-STREAMS_TRIAL&S_CMP=Streams_BDUDWLDTAB', description: 'Streams is a highly scalable analytics platform that allows applications to rapidly analyze, and correlate information as it arrives from thousands of real-time sources at very high data throughput rates.' },
-  { title: 'DB2 Express-C', url: 'http://db2express.com/download?S_TACT=DB2UDWLDTAB', description: 'Download the free version of IBM DB2 Data Servers. <br />DB2 Express-C is free to develop, deploy, and distribute!' },
+  { title: 'BigInsights Basic Edition', url: 'https://www14.software.ibm.com/webapp/iwm/web/preLogin.do?source=swg-ibmibee&S_CMP=BigI_BDUDWLDTAB', description: 'BigInsights Basic Edition is the free version of BigInsights which includes Hadoop and other additional resources.', logo: '/img/logo-biginsights-basic-edition.png' },
+  { title: 'Streams', url: 'https://www14.software.ibm.com/webapp/iwm/web/preLogin.do?source=SWG-STREAMS_TRIAL&S_CMP=Streams_BDUDWLDTAB', description: 'Streams is a highly scalable analytics platform that allows applications to rapidly analyze, and correlate information as it arrives from thousands of real-time sources at very high data throughput rates.', logo: '/img/logo-streams.png' },
+  { title: 'DB2 Express-C', url: 'http://db2express.com/download?S_TACT=DB2UDWLDTAB', description: 'Download the free version of IBM DB2 Data Servers. <br />DB2 Express-C is free to develop, deploy, and distribute!', logo: '/img/logo-db2-express-c.png' },
   { title: 'Data Studio', url: 'https://www14.software.ibm.com/webapp/iwm/web/preLogin.do?lang=en_US&source=swg-idssa&S_TACT=DB2UDWLD', description: 'Data Studio is a free Eclipse tool for administration and development.' }
 ]
 
